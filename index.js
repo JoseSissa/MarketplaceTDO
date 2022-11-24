427ffc8cc3a640507e90062cb27c2f199284d75a
Jose Sissa <jose.sissa@gravital.co>
2022-11-24 12:17:17 -0500

    Filtro producto servicio agregado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const departamentos = document.getElementById('departamentos')
 const municipios = document.getElementById('municipios')
+const productoServicio = document.getElementById('producto-servicio')
 
 
 const filtrarDepartamento = (departamento) => {
@@ -13,15 +14,12 @@ const filtrarMunicipio = (municipio) => {
 //     (e.target.id !== "departamentos" && e.target.id !== "municipios") && resetStyles()
 // })
 
-
-
-
-
-
 departamentos.addEventListener('click', (e) => {    
     if (e.target.localName === 'div' && e.target.id === 'departamentos') {
         municipios.classList.remove('select-activate')
         municipios.children[0].classList.remove('options-activate')
+        productoServicio.classList.remove('select-activate')
+        productoServicio.children[0].classList.remove('options-activate')
         e.target.classList.toggle('select-activate');
         e.target.children[0].classList.toggle('options-activate');
     }
@@ -41,6 +39,8 @@ municipios.addEventListener('click', (e) => {
     if(e.target.localName === 'div' && e.target.id === 'municipios') {
         departamentos.classList.remove('select-activate')
         departamentos.children[0].classList.remove('options-activate')
+        productoServicio.classList.remove('select-activate')
+        productoServicio.children[0].classList.remove('options-activate')
         e.target.classList.toggle('select-activate');
         e.target.children[0].classList.toggle('options-activate');
     }
@@ -52,6 +52,28 @@ municipios.addEventListener('click', (e) => {
         if(e.target.localName === 'button' && e.target.value === "todos") {
             municipios.classList.remove('option-selected')
             municipios.children[1].innerText = "";
+        }
+        // filtrarMunicipio(e.target.innerText)
+    }
+    
+})
+productoServicio.addEventListener('click', (e) => {
+    if(e.target.localName === 'div' && e.target.id === 'producto-servicio') {
+        departamentos.classList.remove('select-activate')
+        departamentos.children[0].classList.remove('options-activate')
+        municipios.classList.remove('select-activate')
+        municipios.children[0].classList.remove('options-activate')
+        e.target.classList.toggle('select-activate');
+        e.target.children[0].classList.toggle('options-activate');
+    }
+    if(e.target.localName === 'button') {
+        productoServicio.children[1].innerText = e.target.innerText;
+        productoServicio.classList.remove('select-activate')
+        productoServicio.classList.add('option-selected')
+        productoServicio.children[0].classList.remove('options-activate')
+        if(e.target.localName === 'button' && e.target.value === "todos") {
+            productoServicio.classList.remove('option-selected')
+            productoServicio.children[1].innerText = "";
         }
         // filtrarMunicipio(e.target.innerText)
     }
